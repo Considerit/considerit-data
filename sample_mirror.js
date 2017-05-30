c57d4fc755b25cad3e2ebee346d680aa267b7e7c
Travis Kriplean <tkriplean@gmail.com>
2017-05-30 15:42:59 -0700

    (possibly working) upgrade to statebus 6
    having trouble testing it locally

--- sample_mirror.js
+++ sample_mirror.js
@@ -1,51 +1,10 @@
+// Run this server so that client apps can connect to it and access data
+bus = require('statebus').serve({
+    port: 9375
+})
 
-// The {subdomain}.consider.it whose data you want to replicate
-var subdomain = process.argv[2] || 'bitcoinclassic'
 
-var bus = require('statebus/server')()
-var request = require('request')
-var history = {}
-
-// Set to true if you want this node to poll the considerit server every 30 min
-// for new data
-var continuous = true
-
-// The data that we want to fetch from considerit
-keys = ['/proposals?all_points=true', '/subdomain', '/users']
-// The all_points=true is so that the /proposals request includes all the published pro/con
-// points for each returned proposal. In normal operation, the points are withheld, but 
-// accessible at /page/{proposal.slug}
-
-if (continuous)
-    setInterval(function () {
-        refresh()
-    }, 30 * 60 * 1000) // please keep this value respectful
-
-refresh()
-
-// Requests all the data we've specified from the considerit server
-function refresh() {
-    for (idx in keys) 
-        http_fetch(keys[idx])
-
-    // Note when the data was last replicated. Will be available in any client applications.
-    refreshed = fetch('/refreshed')
-    if (!refreshed.refreshes)
-        refreshed.refreshes = []
-    refreshed.refreshes.unshift( new Date().getTime() )
-    save(refreshed)
-}
-
-function http_fetch (key) {
-    request({ url: 'https://' + subdomain + '.consider.it' + key,
-              headers: {Accept:'application/json', 'X-Requested-With':'XMLHttpRequest'} },
-
-            function (err, response, body) {
-                bus.pub(JSON.parse(body))
-            })
-}
-
-bus('*').on_pub = function (obj) {
+bus('*').on_save = function (obj) {
     var key = obj.key
     if (!(key in history))
         history[key] = []
@@ -59,7 +18,55 @@ bus('*').on_pub = function (obj) {
     }
 }
 
+///////////////////////////////////////
+// Synchronize with https://consider.it
+/////////
+
+// The {subdomain}.consider.it whose data you want to replicate
+var subdomain = process.argv[2] || 'bitcoinclassic'
+
+var request = require('request')
+var history = {}
+
+// Set to true if you want this node to poll the considerit server every 30 min
+// for new data
+var continuous = true
+
+// The data that we want to fetch from considerit
+keys = ['/proposals?all_points=true', '/subdomain', '/users']
+// The all_points=true is so that the /proposals request includes all the published pro/con
+// points for each returned proposal. In normal operation, the points are withheld, but 
+// accessible at fetch('/page/{proposal.slug}')
+
+
+// Requests all the data we've specified from the considerit server
+function refresh() {
+    for (idx in keys) 
+        http_fetch(keys[idx])
+
+    // Note when the data was last replicated. Will be available in any client applications.
+    refreshed = bus.fetch('/refreshed')
+    if (!refreshed.refreshes)
+        refreshed.refreshes = []
+    refreshed.refreshes.unshift( new Date().getTime() )
+    bus.save(refreshed)
+}
+
+function http_fetch (key) {
+    request({ url: 'https://' + subdomain + '.consider.it' + key,
+              headers: {Accept:'application/json', 'X-Requested-With':'XMLHttpRequest'} },
+
+            function (err, response, body) {
+                bus.save.fire(JSON.parse(body))
+            })
+}
+
 function matches (a, b) { return false }
 
-// Run this server so that client apps can connect to it and access data
-bus.serve({port: 9375})
+if (continuous)
+    setInterval(function () {
+        refresh()
+    }, 30 * 60 * 1000) // please keep this value respectful
+
+refresh()
+
